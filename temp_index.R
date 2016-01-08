bb0d8554adb4fac49aeb905c9a21c8a06cfebe8c
Cloud User <ec2-user@ip-172-31-28-122.ec2.internal>
2016-01-08 17:19:37 -0500

    Working function to read temperature and compute anomalies

---
+++ temp_index.R
@@ -0,0 +1,41 @@
+
+library(zoo) 
+
+# read city temperature data. All data start from 1/1/1995
+# return two time series in a list
+# Usage examples: 
+# data=get_city_temp("NYNEWYOR")
+# plot(data$clim)
+# plot(data$anom)
+# data2=get_city_temp("MIFLINT")
+# length(data2$clim)
+
+get_city_temp <- function(cityname) { 
+  filename = paste("UDayton/", cityname, ".txt", sep='') 
+  # get temperature field, and convert from deg-F to deg-C
+  data = read.table(filename, sep="", header = F, na.strings='-99')
+  temp = na.approx(data$V4) 
+  temp = (temp - 32) * 5 / 9 
+  
+  # get seasonal climatology, and duplicate to the same length as original data 
+  nyr = floor(length(temp)/365) 
+  tmp2d = matrix(temp[1:(nyr*365)], nrow=nyr, ncol=365, byrow=T) 
+  clim = colMeans(tmp2d) 
+  longclim =array(clim, dim=c(1, length(temp)))[1, ] 
+
+  # anomalies 
+  anom = temp - longclim
+  anom.ts = ts(anom, start=c(1995, 1), frequency = 365)
+  clim.ts = ts(longclim, start=c(1995, 1), frequency = 365)
+  list(clim=clim.ts, anom=anom.ts) 
+
+}
+
+
+
+ 
+
+
+
+
+
