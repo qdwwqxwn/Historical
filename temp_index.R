70eb96c271b9feef2b2c5aa7425edc1a17fec8d7
Cloud User <ec2-user@ip-172-31-28-122.ec2.internal>
2016-01-09 17:41:53 -0500

    Handle all the data as zoo objects.

--- temp_index.R
+++ temp_index.R
@@ -2,7 +2,7 @@
 library(zoo) 
 
 # read city temperature data. All data start from 1/1/1995
-# return two time series in a list
+# return a zoo object with "clim" and "anom" 
 # Usage examples: 
 # data=get_city_temp("NYNEWYOR")
 # plot(data$clim)
@@ -18,27 +18,26 @@ library(zoo)
 get_city_temp <- function(cityname) { 
   filename = paste("UDayton/", cityname, ".txt", sep='') 
   # get temperature field, and convert from deg-F to deg-C
-  data = read.table(filename, sep="", header = F, na.strings='-99')
+  # data = read.table(filename, sep="", header = F, na.strings='-99')
   
   # read into zoo 
-  #data=read.zoo(filename, index=1:3, format="%m %d %Y", na.strings='-99')
-  # temp = as.ts(data) 
+  data=read.zoo(filename, index=1:3, format="%m %d %Y", na.strings='-99')
 
-  temp = na.approx(data$V4) 
-  temp = (temp - 32) * 5 / 9 
+  data = na.approx(data) 
+  temp = (data - 32) * 5 / 9 
   
   # get seasonal climatology, and duplicate to the same length as original data 
   nyr = floor(length(temp)/365) 
   tmp2d = matrix(temp[1:(nyr*365)], nrow=nyr, ncol=365, byrow=T) 
-  clim = colMeans(tmp2d) 
-  longclim =array(clim, dim=c(1, length(temp)))[1, ] 
+  sclim = colMeans(tmp2d) 
+  longclim =array(sclim, dim=c(1, length(temp)))[1, ] 
+  # convert to zoo
+  clim = as.zoo(longclim)
+  time(clim) = time(temp) 
 
   # anomalies 
-  anom = temp - longclim
-  anom.ts = ts(anom, start=c(1995, 1), frequency = 365)
-  clim.ts = ts(longclim, start=c(1995, 1), frequency = 365)
-  list(clim=clim.ts, anom=anom.ts) 
-
+  anom = temp - clim 
+  merge(clim, anom) 
 }
 
 
