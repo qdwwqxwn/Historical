cb147c953533953b0d8ae00930a3a6731f19c913
Cloud User <ec2-user@ip-172-31-28-122.ec2.internal>
2016-01-09 18:31:07 -0500

    Completed and tested. To be turned into a function.

--- temp_index.R
+++ temp_index.R
@@ -1,6 +1,17 @@
 
 library(zoo) 
 
+
+# get city population data 
+# returns dataframe with Rank, City Name, and Population
+get_cities <- function( )  { 
+  data = read.csv("Cities/PEP_2014_PEPANNRSIP.US12A_with_ann.csv", skip=2, header=F)  
+  #select Rank, City Name, and 2014 Population
+  b=subset(data, select=c(V6, V8, V14)) 
+  colnames(b)=c("Rank", "City", "Pop")
+  cities=b
+} 
+
 # read city temperature data. All data start from 1/1/1995
 # return a zoo object with "clim" and "anom" 
 # Usage examples: 
@@ -39,6 +50,58 @@ get_city_temp <- function(cityname) {
   anom = temp - clim 
   merge(clim, anom) 
 }
+
+
+# Top 10 city names 
+#   Rank                            City     Pop
+#1     1         New York city, New York 8438379
+#2     2    Los Angeles city, California 3897940
+#3     3          Chicago city, Illinois 2722307
+#4     4             Houston city, Texas 2203806
+#5     5 Philadelphia city, Pennsylvania 1556052
+#6     6           Phoenix city, Arizona 1512442
+#7     7         San Antonio city, Texas 1411766
+#8     8      San Diego city, California 1359844
+#9     9              Dallas city, Texas 1260725
+#10   10       San Jose city, California 1003821   <-- temperature data unavaiable 
+#11   11                   Austin city, Texas  887124
+#12   12           Jacksonville city, Florida  844014
+#13   13       San Francisco city, California  841138
+#14   14 Indianapolis city (balance), Indiana  843375
+#15   15                  Columbus city, Ohio  823536
+#16   16               Fort Worth city, Texas  794055 <-- temperature data unavaiable
+#17   17       Charlotte city, North Carolina  793951
+
+# Temperature city names
+top15 = c("NYNEWYOR", "CALOSANG", "ILCHICAG", "TXHOUSTO", "PAPHILAD", 
+          "AZPHOENI", "TXSANANT", "CASANDIE", "TXDALLAS", "TXAUSTIN", 
+          "FLJACKSV", "CASANFRA", "ININDIAN", "OHCOLMBS", "NCCHARLO") 
+ltop15 = c("New York city, New York", 
+           "Los Angeles city, California",       
+                 "Chicago city, Illinois",       
+                    "Houston city, Texas",       
+        "Philadelphia city, Pennsylvania",       
+                  "Phoenix city, Arizona",       
+                "San Antonio city, Texas",       
+             "San Diego city, California",       
+                     "Dallas city, Texas",       
+                          "Austin city, Texas",       
+                  "Jacksonville city, Florida",       
+              "San Francisco city, California",       
+        "Indianapolis city (balance), Indiana",       
+                         "Columbus city, Ohio", 
+              "Charlotte city, North Carolina") 
+
+cities = get_cities() # get all the cities' pop 
+
+tindex = 0 
+for ( i in 1:length(top15) )  { 
+  temp = get_city_temp( top15[i] ) 
+  anom = temp$anom
+  pop = subset(cities, City==ltop15[i])$Pop 
+  tindex = tindex + anom * pop / 1000000    
+} 
+
 
 
 
