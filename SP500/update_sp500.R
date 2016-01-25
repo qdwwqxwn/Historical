07b57d104376dcd20b050ec8b14f773835ae8cb4
Cloud User <ec2-user@ip-172-31-28-122.ec2.internal>
2016-01-25 18:51:47 -0500

    tested working

---
+++ SP500/update_sp500.R
@@ -0,0 +1,32 @@
+
+# download and save SP500, from whatever is currently available  
+# save to directory 'updated-yyyy-mm-dd'  
+
+library(zoo)
+library("quantmod")
+
+#create directory 
+
+ymd=format(Sys.time(), "%Y-%m-%d") 
+dir=paste("updated-", ymd, sep='')
+dir.create(dir, showWarnings = FALSE) 
+
+# get ticker from the old files 
+files=list.files("quantquote_daily_sp500_83986/daily/", pattern="^table_")
+
+nfiles = length(files) 
+
+for (i in 1:length(files) ) { 
+
+  ticker = sub(".csv$", "", sub("^table_", "", files[i]))
+  cat("Doing ", toupper(ticker), "\n")  
+  data=NULL
+  try( data <- getSymbols(toupper(ticker), auto.assign=FALSE) )  
+  if ( !is.null(data) ) { 
+    write.zoo( data, paste(dir, "/", files[i], sep=''), sep=',') 
+  } else {   # only save valid data
+    cat("Doing ", toupper(ticker), " failed \n")  
+  }
+
+} 
+  
